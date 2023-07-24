45e8d17cd6a94196c909bcadf5647bca66dba2ae
Rushil <rushilbhuptani1088@gmail.com>
2023-07-24 16:07:18 +0530

    add created date column

--- RE360.API/DBModels/Execution.cs
+++ RE360.API/DBModels/Execution.cs
@@ -10,7 +10,7 @@ namespace RE360.API.DBModels
         public int ID { get; set; }
         public int PID { get; set; }
         public string? SignedOnBehalfOfTheAgent { get; set; }
-
+        public DateTime? CreatedDate { get; set; }
         public DateTime? SignedOnBehalfOfTheAgentDate { get; set; }
         public string? SignedOnBehalfOfTheAgentTime { get; set; }
         public string? AgentToSignHere { get; set; }
