32cdbb66e4486167a36ba711a8ad5e079323bcfe
Rushil <rushilbhuptani1088@gmail.com>
2023-08-28 13:06:25 +0530

    Changes  realated to Pdf

--- RE360.API/Models/PropertyInformationDetailViewModel.cs
+++ RE360.API/Models/PropertyInformationDetailViewModel.cs
@@ -26,4 +26,46 @@ namespace RE360.API.Models
         public string? Remarks { get; set; }
         public int Count { get; set; }
     }
+
+    public class PropertyAttributeTypeWithLegalParticularDetailModel
+    {
+        public int PropertyAttributeTypeID { get; set; }
+        public string? Name { get; set; }
+        public int PropertyAttributeID { get; set; }
+        public string? PropertyAttributeName { get; set; }
+        int PID { get; set; }
+        int TitleTypeId { get; set; }
+        int ParticularTypeID { get; set; }
+        public bool Checkbox { get; set; }
+
+    }
+    public class PropertyAttributeTypeWithMethodOfSaleModel
+    {
+        public int PropertyAttributeTypeID { get; set; }
+        public string? Name { get; set; }
+        public int PropertyAttributeID { get; set; }
+        public string? PropertyAttributeName { get; set; }
+        int PID { get; set; }
+        public bool Checkbox { get; set; }
+        public int AgencyTypeID { get; set; }
+        public string? AgencyOtherTypeRemark { get; set; }
+        public int MethodOfSaleID { get; set; }
+        public decimal? Price { get; set; }
+        public string? PriceRemark { get; set; }
+        public DateTime? AuctionDate { get; set; }
+        public string? AuctionTime { get; set; }
+        public string? AuctionVenue { get; set; }
+        public string? Auctioneer { get; set; }
+        public DateTime? TenderDate { get; set; }
+        public string? TenderTime { get; set; }
+        public DateTime? DeadLineDate { get; set; }
+        public string? DeadLineTime { get; set; }
+        public bool IsMortgageeSale { get; set; }
+        public bool IsAsIs { get; set; }
+        public bool IsAuctionUnlessSoldPrior { get; set; }
+        public bool IsTenderUnlessSoldPrior { get; set; }
+        public bool IsAuctionOnSite { get; set; }
+        public string? TenderVenue { get; set; }
+
+    }
 }
